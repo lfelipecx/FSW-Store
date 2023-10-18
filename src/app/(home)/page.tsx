f8a5a5f3acb9c2aa735cd376ccdcbe40837f3d7c
Luiz Felipe <lfelipecx@gmail.com>
2023-10-18 15:07:44 -0300

    feat: add third promo banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,30 +22,41 @@ export default async function Home() {
   })
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner 
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês!"
       />
 
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories />
       </div>
 
-      <div className="mt-7">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
 
-      <PromoBanner 
-        src="/banner-home-02.png"
-        alt="Até 55% de desconto em mouses!"
-      />
+      <div>
+        <PromoBanner 
+          src="/banner-home-02.png"
+          alt="Até 55% de desconto em mouses!"
+        />
+      </div>
 
-      <div className="mt-7">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
       </div>
+
+      <div>
+        <PromoBanner 
+          src="/banner-home-03.png"
+          alt="Até 20% de desconto em fones!"
+        />
+      </div>
+
+      
     </div>
   )
 }
